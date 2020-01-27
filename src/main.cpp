1f720b2fb73c3c483d77d35d2c49e95522a851be
reedtaylor <reed_github@reedtaylor.com>
2020-01-26 23:59:40 -0500

    websockets exist but w/ race condition

--- src/main.cpp
+++ src/main.cpp
@@ -66,7 +66,70 @@ void onUpload(AsyncWebServerRequest *request, String filename, size_t index,
 
 void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
              AwsEventType type, void *arg, uint8_t *data, size_t len) {
-  // Handle WebSocket event
+  if(type == WS_EVT_CONNECT){
+    //client connected
+    Serial_USB.printf("ws[%s][%u] connect\n", server->url(), client->id());
+    client->printf("Hello Client %u :)", client->id());
+    client->ping();
+  } else if(type == WS_EVT_DISCONNECT){
+    //client disconnected
+    Serial_USB.printf("ws[%s][%u] disconnect: %u\n", server->url(), client->id());
+  } else if(type == WS_EVT_ERROR){
+    //error was received from the other end
+    Serial_USB.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+  } else if(type == WS_EVT_PONG){
+    //pong message was received (in response to a ping request maybe)
+    Serial_USB.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len)?(char*)data:"");
+  } else if(type == WS_EVT_DATA){
+    //data packet
+    AwsFrameInfo * info = (AwsFrameInfo*)arg;
+    if(info->final && info->index == 0 && info->len == len){
+      //the whole message is in a single frame and we got all of it's data
+      Serial_USB.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT)?"text":"binary", info->len);
+      if(info->opcode == WS_TEXT){
+        data[len] = 0;
+        Serial_USB.printf("%s\n", (char*)data);
+      } else {
+        for(size_t i=0; i < info->len; i++){
+          Serial_USB.printf("%02x ", data[i]);
+        }
+        Serial_USB.printf("\n");
+      }
+      if(info->opcode == WS_TEXT)
+        client->text("I got your text message");
+      else
+        client->binary("I got your binary message");
+    } else {
+      //message is comprised of multiple frames or the frame is split into multiple packets
+      if(info->index == 0){
+        if(info->num == 0)
+          Serial_USB.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+        Serial_USB.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+      }
+
+      Serial_USB.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT)?"text":"binary", info->index, info->index + len);
+      if(info->message_opcode == WS_TEXT){
+        data[len] = 0;
+        Serial_USB.printf("%s\n", (char*)data);
+      } else {
+        for(size_t i=0; i < len; i++){
+          Serial_USB.printf("%02x ", data[i]);
+        }
+        Serial_USB.printf("\n");
+      }
+
+      if((info->index + len) == info->len){
+        Serial_USB.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+        if(info->final){
+          Serial_USB.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+          if(info->message_opcode == WS_TEXT)
+            client->text("I got your text message");
+          else
+            client->binary("I got your binary message");
+        }
+      }
+    }
+  }
 }
 #endif  // ifdef WEBSERVER
 
@@ -122,6 +185,7 @@ void setup() {
       return request->requestAuthentication();
     request->send(200, "text/plain", "Login Success!");
   });
+ 
   // Simple Firmware Update Form
   server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "text/html",
@@ -186,9 +250,11 @@ void loop() {
     ESP.restart();
   }
 
-  static char temp[128];
-  sprintf(temp, "Seconds since boot: %lu", millis() / 1000);
-  events.send(temp, "time");  // send event "time"
+  ws.cleanupClients();
+
+  // static char temp[128];
+  // sprintf(temp, "Seconds since boot: %lu", millis() / 1000);
+  // events.send(temp, "time");  // send event "time"
 
 #endif  // WEBSERVER
 
@@ -207,7 +273,21 @@ void loop() {
     // Send to serial interfaces
     Serial_BLE.write(buf, bufIndex);
     Serial_USB.write(buf, bufIndex);
+
+#ifdef WEBSERVER
+    if (ws.count()) {
+      if (ws.availableForWriteAll()) {
+        ws.binaryAll(buf, bufIndex);
+      } else {
+        // TODO we should sniff around and evict clients in this situation
+        // to avoid the tragedy of the commons or whatever
+        Serial_USB.println("Websockets blocking");
+      }
+    } 
+#endif // WEBSERVER
+
   }
+
 
   // Bridge BLE messages to DE1
   if (Serial_BLE.available()) {
