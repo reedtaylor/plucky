cc9895ad4272425a8d28a0d53b2141e4978800cc
reedtaylor <reed_github@reedtaylor.com>
2020-01-31 10:34:51 -0500

    readbuffer refactor complete, TCP working

--- src/main.cpp
+++ src/main.cpp
@@ -29,89 +29,58 @@
 
 /*************************  WiFi & TCP Config *******************************/
 #define WIFI
+#ifdef WIFI
 
-/***
-// -- Initial name of the Thing. Used e.g. as SSID of the own Access Point.
-const char thingName[] = "espresso_plucky";
+#define TCP
+#ifdef TCP
+const uint16_t maxTcpClients = 8;
+const int tcpPort = 9090;
+#endif // TCP
+#endif
+// WIFI
 
-// -- Initial password to connect to the Thing, when it creates an own Access Point.
-const char wifiInitialApPassword[] = "decentDE1";
-****/
-
-
-
-#define MAX_TCP_CLIENTS 10
-#define TCP_PORT 9090
-
-/// end config ///////////////////////////////////////////////////////////
+// / end config ///////////////////////////////////////////////////////////
 
 // Enumerate the hardware serial devices
 #include "driver/uart.h"
 HardwareSerial & Serial_USB = Serial;
+uint8_t readBuf_USB[bufferSize];
+uint16_t readBufIndex_USB = 0;
+
 HardwareSerial Serial_DE(SERIAL_DE_UART_NUM);
+uint8_t readBuf_DE[bufferSize];
+uint16_t readBufIndex_DE = 0;
+
 HardwareSerial Serial_BLE(SERIAL_BLE_UART_NUM);
+uint8_t readBuf_BLE[bufferSize];
+uint16_t readBufIndex_BLE = 0;
 
 #ifdef WIFI
+#include <WiFi.h> 
 
-/***
-// Instantiate the web configuration interface
-#include <IotWebConf.h>
-DNSServer dnsServer;
-WebServer webServer(80);
-HTTPUpdateServer httpUpdater;
-
-IotWebConf iotWebConf(thingName, &dnsServer, &webServer, wifiInitialApPassword);
-***/
-
+#ifdef TCP
 // Instantiate the TCP sockets for talking to the machine
-#include <WiFi.h>
-WiFiServer TCPServer(TCP_PORT);
-WiFiClient TCPClient[MAX_TCP_CLIENTS];
+WiFiServer TCPServer(tcpPort);
+WiFiClient TCPClient[maxTcpClients];
+uint8_t readBuf_TCP[maxTcpClients][bufferSize];
+uint16_t readBufIndex_TCP[maxTcpClients];
+#endif // TCP
 
-#endif // ifdef WIFI
-
-bool wifiReady = false;
+#endif // WIFI
 
 void setup() {
-  delay(100);
 
-  /******* Serial initialization ************/
-  Serial_USB.begin(UART_BAUD);
-  Serial_DE.begin(UART_BAUD, SERIAL_PARAM, SERIAL_DE_RX_PIN, SERIAL_DE_TX_PIN);
-  Serial_BLE.begin(UART_BAUD, SERIAL_PARAM, SERIAL_BLE_RX_PIN, SERIAL_BLE_TX_PIN);
+    /******* Serial initialization ************/
+    Serial_USB.begin(UART_BAUD);
+    Serial_DE.begin(UART_BAUD, SERIAL_PARAM, SERIAL_DE_RX_PIN, SERIAL_DE_TX_PIN);
+    Serial_BLE.begin(UART_BAUD, SERIAL_PARAM, SERIAL_BLE_RX_PIN, SERIAL_BLE_TX_PIN);
+    // HW flow contol for the BLE adaptor.
+    // https://github.com/espressif/arduino-esp32/blob/master/tools/sdk/include/driver/driver/uart.h
+    uart_set_hw_flow_ctrl(SERIAL_BLE_UART_NUM, UART_HW_FLOWCTRL_CTS_RTS, 0);
+    uart_set_pin(SERIAL_BLE_UART_NUM, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, SERIAL_BLE_RTS_PIN, SERIAL_BLE_CTS_PIN);
 
-  // Need to make a couple of "low level" esp-idf-esque calls to setup HW flow
-  // contol for the BLE adaptor.  I think this is doable because it's provided
-  // in the driver here:
-  // https://github.com/espressif/arduino-esp32/blob/master/tools/sdk/include/driver/driver/uart.h
-  //
-  // For now only enabling CTS because it's only the BLE adaptor side that might
-  // use this.  May need to force the RTS pin low, we'll see
-  // Reference: esp_err_t uart_set_hw_flow_ctrl(uart_port_t uart_num, uart_hw_flowcontrol_t flow_ctrl, uint8_t rx_thresh);
-  uart_set_hw_flow_ctrl(SERIAL_BLE_UART_NUM, UART_HW_FLOWCTRL_CTS_RTS, 0);
-
-  // Reference: esp_err_t uart_set_pin(uart_port_t uart_num, int tx_io_num, int rx_io_num, int rts_io_num, int cts_io_num);
-  uart_set_pin(SERIAL_BLE_UART_NUM, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, SERIAL_BLE_RTS_PIN, SERIAL_BLE_CTS_PIN);
-
-
-  /******* Wifi initialization ***********/
-#ifdef WIFI
-
-/***
-  
-  // Initialize the web configuration interface
-  iotWebConf.setWifiConnectionCallback(&wifiConnected);
-  iotWebConf.init();
-  // Set up required URL handlers on the web server.
-  webServer.on("/", handleRoot);
-  webServer.on("/config", [] { iotWebConf.handleConfig(); });
-  webServer.onNotFound([]() {
-    iotWebConf.handleNotFound();
-  });
-  Serial.println("Web configuration interface initialized.");
-
-  ***/
-
+    /******* Wifi initialization ***********/
+    #ifdef WIFI
     Serial.println();
     Serial.println();
     Serial.print("Connecting to wifi");
@@ -126,188 +95,141 @@ void setup() {
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
-    wifiReady = true;
+
+    #ifdef TCP
     TCPServer.begin(); // start TCP server
     TCPServer.setNoDelay(true);
     Serial.println("TCP server enabled");
+    #endif // TCP
+    #endif // WIFI
 
-
-#endif // WIFI
-
-  delay(10);
-  Serial_USB.println("Plucky initialization completed.");
+    delay(10);
+    Serial_USB.println("Plucky initialization completed.");
 }
 
 void loop() {
 
-  uint8_t buf[bufferSize];
-  uint16_t bufIndex = 0;
-
-#ifdef WIFI
-
-/***
-  
-  // Handler for the web configuration UI
-  iotWebConf.doLoop();
-***/
-
-  if (wifiReady) {
-    // Check for new incoming connections
-    if (TCPServer.hasClient()) {
-      //find free/disconnected spot
-      for (byte i = 0; i < MAX_TCP_CLIENTS; i++) {
-       if (!TCPClient[i] || !TCPClient[i].connected()) {
-          if (TCPClient[i]) {
-            TCPClient[i].stop();
-         }
-          TCPClient[i] = TCPServer.available();
+#ifdef TCP
+  // Check for new incoming connections
+  if (TCPServer.hasClient()) { // find free/disconnected spot
+      for (byte i = 0; i < maxTcpClients; i ++) {
+        if (!TCPClient[i]) {
+          TCPClient [i] = TCPServer.available();
+          readBufIndex_TCP[i] = 0;
           Serial_USB.print("New TCP client in slot: ");
           Serial_USB.println(i);
           break;
         }
-       if (i == MAX_TCP_CLIENTS - 1 ) {
-          //no free/disconnected spot so reject it
-          WiFiClient TmpserverClient = TCPServer.available();
-          TmpserverClient.stop();
-         Serial_USB.println("Too many TCP clients; new connection dropped");
+        if (i == maxTcpClients - 1) { // no free/disconnected spot so reject it
+            WiFiClient TmpserverClient = TCPServer.available();
+            TmpserverClient.stop();
+            Serial_USB.println("Too many TCP clients; new connection dropped");
         }
       }
-    }
-  }
+  }     
+#endif // TCP
 
-#endif // WIFI
-
-  // Broadcast DE1 messages to controllers
+  // Receive DE1 messages
   if (Serial_DE.available()) {
-    bufIndex = 0;
-    if (Serial_DE.available()) {
-      byte c = Serial_DE.read(); // read char from DE UART
-      Serial_BLE.write(c);
-      Serial_USB.write(c);
+    while (Serial_DE.available() && (readBufIndex_DE < bufferSize)) {
+      readBuf_DE[readBufIndex_DE] = Serial_DE.read();
+      readBufIndex_DE ++;
+      if (readBuf_DE[readBufIndex_DE - 1] == '\n') { 
+        // let's be super deliberate about resetting the readBufIndex lest we forget to do it below
+        uint16_t sendLen = readBufIndex_DE;
+        readBufIndex_DE = 0;
 
-#ifdef WIFI
-      // Send to TCP clients
-      if (wifiReady) {
-        for (byte client_num = 0; client_num < MAX_TCP_CLIENTS; client_num++) {
-         if (TCPClient[client_num]) {
-            Serial_USB.write(c);
-            //TCPClient[client_num].write(c); 
-          } 
+        // Broadcast to Serial interfaces
+        Serial_BLE.write(readBuf_DE, sendLen);
+        Serial_USB.write(readBuf_DE, sendLen);
+
+#ifdef TCP
+        // Broadcast to TCP clients
+        for (uint16_t i = 0; i < maxTcpClients; i ++) {
+          if (TCPClient[i]) {
+              TCPClient[i].write(readBuf_DE, sendLen);
+          }
         }
-      }
-#endif // WIFI
-      
-      if(c == '\n') {
-        //break; // we don't want to get into a state where buffers misalign with the message frames
+#endif // TCP
+
       }
     }
-/***
-    // Send to serial interfaces
-    Serial_BLE.write(buf, bufIndex);
-    Serial_USB.write(buf, bufIndex);
-
-#ifdef WIFI
-    // Send to TCP clients
-    if (wifiReady) {
-      for (byte client_num = 0; client_num < MAX_TCP_CLIENTS; client_num++) {
-        if (TCPClient[client_num]) {
-          TCPClient[client_num].write(buf, bufIndex); 
-        }
-      }
-    }
-#endif // WIFI
-***/
+  } 
+  if (readBufIndex_DE >= bufferSize) {
+      Serial_USB.printf("WARNING: DE Read Buffer Overrun, purging.  Buffer Contents: ");
+      Serial_USB.write(readBuf_DE, bufferSize);
+      Serial_USB.printf("\n");
+      readBufIndex_DE = 0;
   }
 
   // Bridge BLE messages to DE1
   if (Serial_BLE.available()) {
-    bufIndex = 0;
-    while (Serial_BLE.available() && (bufIndex < (bufferSize - 1))) {
-      buf[bufIndex] = Serial_BLE.read();
-      bufIndex++;
-      if(buf[bufIndex-1] == '\n') {
-        break; // we don't want to get into a state where buffers misalign with the message frames
+    while (Serial_BLE.available() && (readBufIndex_BLE < bufferSize)) {
+      readBuf_BLE[readBufIndex_BLE] = Serial_BLE.read();
+      readBufIndex_BLE ++;
+      if (readBuf_BLE[readBufIndex_BLE - 1] == '\n') { 
+        // let's be super deliberate about resetting the readBufIndex lest we forget to do it below
+        uint16_t sendLen = readBufIndex_BLE;
+        readBufIndex_BLE = 0;
+
+        // Send to DE
+        Serial_DE.write(readBuf_BLE, sendLen);
       }
     }
-
-    // Send to DE
-    Serial_DE.write(buf, bufIndex);
   }
-
+  if (readBufIndex_BLE >= bufferSize) {
+    Serial_USB.printf("WARNING: BLE Read Buffer Overrun.  Buffer Contents: ");
+    Serial_USB.write(readBuf_BLE, bufferSize);
+    Serial_USB.printf("\n");
+    readBufIndex_BLE = 0;
+  }
 
   // Bridge USB messages to DE1
   if (Serial_USB.available()) {
-    bufIndex = 0;
-    while (Serial_USB.available() && (bufIndex < (bufferSize - 1))) {
-      buf[bufIndex] = Serial_USB.read();
-      bufIndex++;
-      if(buf[bufIndex-1] == '\n') {
-        break; // we don't want to get into a state where buffers misalign with the message frames
+    while (Serial_USB.available() && (readBufIndex_USB < bufferSize)) {
+      readBuf_USB[readBufIndex_USB] = Serial_USB.read();
+      readBufIndex_USB ++;
+      if (readBuf_USB[readBufIndex_USB - 1] == '\n') { 
+        // let's be super deliberate about resetting the readBufIndex lest we forget to do it below
+        uint16_t sendLen = readBufIndex_USB;
+        readBufIndex_USB = 0;
+
+        // Send to DE
+        Serial_DE.write(readBuf_USB, sendLen);
       }
+    }
+  }
+  if (readBufIndex_USB >= bufferSize) {
+    Serial_USB.printf("WARNING: BLE Read Buffer Overrun.  Buffer Contents: ");
+    Serial_USB.write(readBuf_USB, bufferSize);
+    Serial_USB.printf("\n");
+    readBufIndex_USB = 0;
   }
 
-    // Send to DE
-    Serial_DE.write(buf, bufIndex);
-  }
 
-
-#ifdef WIFI
+#ifdef TCP
   // Bridge TCP messages to DE1
-  if (wifiReady) {
-    for (byte client_num = 0; client_num < MAX_TCP_CLIENTS; client_num++) {
-     if (TCPClient[client_num]) {
-       if (TCPClient[client_num].available()) {
-         bufIndex = 0;
-         while (TCPClient[client_num].available() && (bufIndex < (bufferSize - 1))) {
-           buf[bufIndex] = TCPClient[client_num].read();
-           bufIndex++;
-           if(buf[bufIndex-1] == '\n') {
-              break; // we don't want to get into a state where buffers misalign with the message frames
-            }         
-         }
+  for (byte i = 0; i < maxTcpClients; i ++) {
+    if (TCPClient[i] && TCPClient[i].available()) {
+      while (TCPClient[i].available() && (readBufIndex_TCP[i] < bufferSize)) {
+        readBuf_TCP[i][readBufIndex_TCP[i]] = TCPClient[i].read();
+        readBufIndex_TCP[i]++;
+        if (readBuf_TCP[i][readBufIndex_TCP[i] - 1] == '\n') { 
+          // let's be super deliberate about resetting the readBufIndex lest we forget to do it below
+          uint16_t sendLen = readBufIndex_TCP[i];
+          readBufIndex_TCP[i] = 0;
 
-         // Send to DE
-         Serial_DE.write(buf, bufIndex);
-       }
-     }
-   }
-  }
-#endif // WIFI
-
-
+          // Send to DE
+          Serial_DE.write(readBuf_USB, sendLen);
+        }
+      }
+    }
+    if (readBufIndex_USB >= bufferSize) {
+      Serial_USB.printf("WARNING: BLE Read Buffer Overrun.  Buffer Contents: ");
+      Serial_USB.write(readBuf_USB, bufferSize);
+      Serial_USB.printf("\n");
+      readBufIndex_TCP[i] = 0;
+    } 
+  } 
+#endif // WIFI}}}#endif // WIFI#endif // 0
 }
-
-
-/***
-void wifiConnected()
-{
-  // Initialize the TCP server for talking to the machine
-  Serial.println("Wifi Ready - enabling TCP server");
-  wifiReady = true;
-  TCPServer.begin(); // start TCP server
-  TCPServer.setNoDelay(true);
-}
-***/
-
-/*
-   Web configuration: Handle web requests to "/" path.
-*/
-#if 0
-#ifdef WIFI
-void handleRoot()
-{
-  // Let IotWebConf test and handle captive portal requests.
-  if (iotWebConf.handleCaptivePortal())
-  {
-    // Captive portal request were already served.
-    return;
-  }
-  String s = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
-  s += "<title>Plucky Web Interface</title></head><body>Hello world!";
-  s += "Go to <a href='config'>configure page</a> to change settings.";
-  s += "</body></html>\n";
-
-  webServer.send(200, "text/html", s);
-}
-#endif // WIFI
-#endif // 0
